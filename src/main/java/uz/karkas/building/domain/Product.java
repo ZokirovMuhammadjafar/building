bec43ceec986c656a76311978654f605fd4cc8af
jafar <ibook2025com@gmail.com>
2022-04-08 18:17:31 +0500

    second

---
+++ src/main/java/uz/karkas/building/domain/Product.java
@@ -0,0 +1,12 @@
+package uz.karkas.building.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+public class Product {
+}
---
+++ src/main/java/uz/karkas/building/domain/Project.java
@@ -0,0 +1,13 @@
+package uz.karkas.building.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+public class Project {
+
+}
