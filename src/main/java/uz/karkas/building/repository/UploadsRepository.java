de74acb550be86b02b86aa7a06da55b3407711fb
Abdulfayz <usa.xamidov@gmail.com>
2022-06-07 11:44:49 +0500

    final 🥲

--- src/main/java/uz/karkas/building/repository/UploadsRepository.java
+++ src/main/java/uz/karkas/building/repository/UploadsRepository.java
@@ -6,11 +6,12 @@ import uz.karkas.building.domain.Uploads;
 
 import java.util.Optional;
 
-public interface UploadsRepository extends JpaRepository<Uploads,Integer> {
+public interface UploadsRepository extends JpaRepository<Uploads, Integer> {
     Optional<Uploads> findByPathName(String id);
-    @Query(value = "insert into files(bytes) values ( ?1 ) returning id", nativeQuery = true)
 
+    @Query(value = "insert into files(bytes) values ( ?1 ) returning id", nativeQuery = true)
     Integer savefile(byte[] bytes);
+
     @Query(value = "select bytes from files where id = ?1", nativeQuery = true)
     byte[] take(Integer fileId);
 }
